d36a638235d2ff573d6941bfc2f4c1a06922b26f
Spencer Miskoviak <smiskoviak@gmail.com>
2018-06-24 13:25:27 -0700

    Fix type issues

--- src/create.ts
+++ src/create.ts
@@ -1,21 +1,34 @@
 import { isFunction } from "./utils";
 
-type Config<T> = { [P in keyof T]: ((i: number) => T[P]) | T[P] };
+type Config<T> = { [P in keyof T]: T[P] | ((i: number) => T[P]) };
 
+/**
+ * Create a new factory function. The return value is a function that can be
+ * invoked as many times as needed to create a given type of object. Use the 
+ * config param to define how the object is generated on each invocation.
+ * 
+ * @param config An object that defines how the object should be generated. Each
+ * key can either be a static value, or a function that receives the invocation
+ * count as the only parameter.
+ */
 export function create<Result>(config: Config<Result>): () => Result {
   let invocations = 0;
+
   return (): Result => {
     invocations++;
-    let result = {};
+    let result: Result = {} as any;
 
-    Object.keys(config).forEach(key => {
-      const cur = config[key];
-      if (isFunction(cur)) {
-        result[key] = cur(invocations);
+    for (let key in config) {
+      const value = config[key];
+
+      if (isFunction(value)) {
+        result[key] = value(invocations);
       } else {
-        result[key] = cur;
+        // NOTE: Ideally we can infer this is _not_ a function without the cast.
+        result[key] = value as Result[Extract<keyof Result, string>];
       }
-    });
-    return result as Result;
+    }
+
+    return result;
   };
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 /**
  * Type guard funciton to determine if the argument passed is a function.
- * 
+ *
  * @param functionToCheck value to check if it is a function
  */
 export const isFunction = (
