b637d8c5cd4c6a5a3af7d54047ce64d51f37f7e9
Spencer Miskoviak <smiskoviak@gmail.com>
2018-08-07 20:08:17 -0700

    Add comments in utils about suggested usage of type guards

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,24 @@
 import { DerivedFunction } from "./derive";
 import { Factory, AttributeFunction } from "./define";
 
+// Determine if the function is an internal derive function based on properties
+// define on the function.
 function isDerivedFunction<Base, Output>(
   fn: any
 ): fn is DerivedFunction<Base, Output> {
   return fn && fn.hasOwnProperty("__cooky-cutter-derive");
 }
 
+// Determine if the function is an internal factory function based on properties
+// define on the function.
 function isFactoryFunction<Base>(fn: any): fn is Factory<Base> {
   return fn && fn.hasOwnProperty("__cooky-cutter-factory");
 }
 
+// Determine if the function is an attribute function. Since this is end-user
+// defined there is not a great way to know for sure if it exactly matches
+// an attribute function, but this is a best guess. This should be used after
+// all other function type checks.
 function isAttributeFunction<T>(fn: any): fn is AttributeFunction<T> {
   return fn && {}.toString.call(fn) === "[object Function]";
 }
