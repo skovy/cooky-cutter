1eab277ebfa7786cd7053d0000c2e565cc131cde
Spencer Miskoviak <smiskoviak@gmail.com>
2018-08-07 20:03:00 -0700

    Add internal comments for the compute method
    This method is intended to be internal to the package (private), but it needs to be recursively called so it's a standalone method and also needs to be called by both define and extend

--- src/compute.ts
+++ src/compute.ts
@@ -5,6 +5,10 @@ import {
 } from "./utils";
 import { Config } from "./define";
 
+// Given a key, the configuration object (with overrides already applied) and
+// the end result object, compute the current value for the given key and write
+// that value to the result object. Optionally track the path values are
+// computed along in cases where it's possible to define circular dependencies.
 function compute<
   Key extends keyof Result,
   Values extends Config<Result>,
@@ -18,6 +22,9 @@ function compute<
 ) {
   const value = values[key];
 
+  // In essence, this is "exhaustively" checking for each type of attribute that
+  // can be defined for a given key. Unfortunately it's not truly exhaustive,
+  // but would be great to update this to do true exhaustive type checking.
   if (isDerivedFunction<Result, Result[Key]>(value)) {
     result[key] = value(result, values, invocations, path);
   } else if (isFactoryFunction<Result[Key]>(value)) {
