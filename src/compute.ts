1c81d20b023146d976da59f480de956948b71d5d
Spencer Miskoviak <smiskoviak@gmail.com>
2019-05-17 19:13:46 -0700

    Add the basics for minimally warning about hard-coded values

--- src/compute.ts
+++ src/compute.ts
@@ -32,8 +32,27 @@ function compute<
   } else if (isAttributeFunction<Result[Key]>(value)) {
     result[key] = value(invocations);
   } else {
+    warnAboutHardCodedValues(key, value);
     result[key] = value as Result[Key];
   }
 }
+
+/**
+ * Explicitly setting an object or array as a value in a factory can lead to
+ * really challenging and subtle bugs since they will be shared across all
+ * instances of a factory. Check for objects and arrays and by default display
+ * a warning.
+ */
+const warnAboutHardCodedValues = <Key, Value>(key: Key, value: Value) => {
+  if (Array.isArray(value)) {
+    console.warn(
+      `\`${key}\` contains a hard-coded array. It will be shared across all instances of this factory. Consider using a factory function.`
+    );
+  } else if (typeof value === "object" && value !== null) {
+    console.warn(
+      `\`${key}\` contains a hard-coded object. It will be shared across all instances of this factory. Consider using a factory function.`
+    );
+  }
+};
 
 export { compute };
